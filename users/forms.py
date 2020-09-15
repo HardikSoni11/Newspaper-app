7e90a784b0b373fd20240e717859a45cd8a24966
HardikSoni11 <hsoni1096@gmail.com>
2020-09-16 01:31:33 +0530

    Auth added with django

--- users/forms.py
+++ users/forms.py
@@ -6,9 +6,12 @@ from .models import CustomUser
 class CustomUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm):
         model = CustomUser
-        fields = UserCreationForm.Meta.fields + ('age',)
+        # fields = UserCreationForm.Meta.fields + ('age',) # meta here -> default && additional age field added
+        fields = ('username', 'email', 'age') ## all fields added by overselves **default password field will 
+        # still appear so use this way only!!
 
 class CustomUserChangeForm(UserChangeForm):
     class Meta(UserChangeForm):
         model = CustomUser
-        fields = UserChangeForm.Meta.fields
+        # fields = UserChangeForm.Meta.fields # same as above
+        fields = ('username', 'email', 'age')
